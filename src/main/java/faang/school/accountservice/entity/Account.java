3dba278391487c04ff612597845c4d8912cc5ba1
nadir <den4ik.strelok@gmail.com>
2024-06-20 20:37:29 +0300

    Добавил сущность платежного счета

---
+++ src/main/java/faang/school/accountservice/entity/Account.java
@@ -0,0 +1,70 @@
+package faang.school.accountservice.entity;
+
+import faang.school.accountservice.enums.AccountStatus;
+import faang.school.accountservice.enums.AccountType;
+import faang.school.accountservice.enums.Currency;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "number", length = 20, nullable = false)
+    private String number;
+
+    @Column(name = "owner_user_id")
+    private long ownerUserId;
+
+    @Column(name = "owner_user_id")
+    private long ownerProjectId;
+
+    @Column(name = "type", length = 128, nullable = false)
+    private AccountType type;
+
+    @Column(name = "currency", length = 3, nullable = false)
+    private Currency currency;
+
+    @Column(name = "status", length = 32, nullable = false)
+    private AccountStatus status;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "closed_at")
+    private LocalDateTime closedAt;
+
+    @Version()
+    @Column(name = "version", nullable = false)
+    private long version;
+}
---
+++ src/main/java/faang/school/accountservice/enums/AccountType.java
@@ -0,0 +1,9 @@
+package faang.school.accountservice.enums;
+
+public enum AccountType {
+    CURRENT_ACCOUNT,
+    SAVINGS_ACCOUNT,
+    INVESTMENT_ACCOUNT,
+    PERSONAL_ACCOUNT,
+    CREDIT_ACCOUNT
+}
