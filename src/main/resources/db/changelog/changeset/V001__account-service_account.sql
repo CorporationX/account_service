fd91477b58fad2e428a2caa8b0e8cdfa36c6bd5b
nadir <den4ik.strelok@gmail.com>
2024-06-20 15:56:47 +0300

    Написал миграцию для создания таблицы account

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,15 @@
--- Write your sql migration here!
+CREATE TABLE IF NOT EXISTS account (
+    id BIGSERIAL PRIMARY KEY,
+    number VARCHAR(20) NOT NULL,
+    owner_user_id BIGINT,
+    owner_project_id BIGINT,
+    type VARCHAR(128) NOT NULL,
+    currency VARCHAR(3) NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    closed_at TIMESTAMP,
+    version BIGINT NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS account_user_project ON account (owner_user_id, owner_project_id);
---
+++ src/main/resources/db/changelog/drop.sql
@@ -0,0 +1,4 @@
+DROP INDEX account_user_project;
+DROP TABLE account;
+
+DELETE FROM databasechangelog;
