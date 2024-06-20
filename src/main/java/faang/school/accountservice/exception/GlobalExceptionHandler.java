6dc8ca423a2ed3593958593f13789e1545474936
nadir <den4ik.strelok@gmail.com>
2024-06-21 02:40:14 +0300

    Добавил свойство уникальности для поля номера счета

--- src/main/java/faang/school/accountservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/accountservice/exception/GlobalExceptionHandler.java
@@ -1,47 +1,50 @@
 package faang.school.accountservice.exception;
 
+import faang.school.accountservice.dto.Error;
 import jakarta.validation.ConstraintViolationException;
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DataValidationException.class)
     public ResponseEntity<Error> handleDataValidationException(DataValidationException e) {
-        return ResponseEntity.badRequest().body(new Error(e.getMessage()));
+        return ResponseEntity.badRequest().body(new Error("Wrong data", e.getMessage()));
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Error> handleNoSuchElementException(NoSuchElementException e) {
-        return ResponseEntity.badRequest().body(new Error(e.getMessage()));
+        return ResponseEntity.badRequest().body(new Error("No such element", e.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Error> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseEntity.internalServerError().body(new Error("Data operation failed", e.getMessage()));
     }
 
     @ExceptionHandler(DataOperationException.class)
     public ResponseEntity<Error> handleDataOperationException(DataOperationException e) {
-        return ResponseEntity.internalServerError().body(new Error(e.getMessage()));
+        return ResponseEntity.internalServerError().body(new Error("Data operation failed", e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        Map<String, String> exceptionMap = methodArgumentNotValidException.getBindingResult().getAllErrors().stream()
-                .collect(Collectors.toMap(
-                        error -> ((FieldError) error).getField(),
-                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
-                ));
-        return ResponseEntity.badRequest().body(exceptionMap);
+    public ResponseEntity<List<Error>> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        List<Error> exceptionList = methodArgumentNotValidException.getBindingResult().getAllErrors().stream()
+                .map(error -> new Error("Invalid data passed", error.getDefaultMessage()))
+                .toList();
+        return ResponseEntity.badRequest().body(exceptionList);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Error> handleConstraintViolationException(ConstraintViolationException e) {
-        return ResponseEntity.badRequest().body(new Error(e.getMessage()));
+        return ResponseEntity.badRequest().body(new Error("Constraint violated", e.getMessage()));
     }
 }
