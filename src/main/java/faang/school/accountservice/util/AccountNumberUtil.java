935df13fc3f77ffeea97a3a82d3c83b2a1efb4bb
Tem <tipokruto014@gmail.com>
2024-06-23 15:47:11 +0300

    add util validation

--- src/main/java/faang/school/accountservice/util/AccountNumberUtil.java
+++ src/main/java/faang/school/accountservice/util/AccountNumberUtil.java
@@ -7,6 +7,8 @@ public class AccountNumberUtil {
 
     public String getAccountNumber(int code, int length, long count) {
 
+        validatePositive(code, length, count);
+
         String codeStr = String.valueOf(code);
         String countStr = String.valueOf(count);
 
@@ -16,12 +18,22 @@ public class AccountNumberUtil {
         int dif = length - codeStr.length() - countStr.length();
 
         if (dif < 0) {
-            throw new IllegalArgumentException("count is greater than length");
+            throw new IllegalArgumentException("Count is greater than length");
         }
 
         builder.append("0".repeat(dif));
         builder.append(count);
 
         return builder.toString();
+    }
+
+    private void validatePositive(int code, int length, long count) {
+        if (code < 0) {
+            throw new IllegalArgumentException("Code must be greater that zero");
+        } else if (length < 0) {
+            throw new IllegalArgumentException("Length must be greater that zero");
+        } else if (count < 0) {
+            throw new IllegalArgumentException("count must be greater that zero");
+        }
     }
 }
