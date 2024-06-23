8c125184f3315884e279c12cc6db45282a947dd6
Ilya <ilya-ermakov.2003@yandex.ru>
2024-06-23 21:28:31 +0300

    создание миграции

---
+++ src/main/java/faang/school/accountservice/entity/Balance.java
@@ -0,0 +1,47 @@
+package faang.school.accountservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "balance")
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "authorization_balance", nullable = false)
+    private BigDecimal authorizationBalance;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Version()
+    @Column(name = "version")
+    private long version;
+
+}
