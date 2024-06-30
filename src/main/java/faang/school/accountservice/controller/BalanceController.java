d18f09e6eadcd055fdb0371a75138c37259a7893
Ilya <ilya-ermakov.2003@yandex.ru>
2024-06-30 09:09:46 +0300

    добавление логики программы по получению, добавления и обновления баланса

---
+++ src/main/java/faang/school/accountservice/controller/BalanceController.java
@@ -0,0 +1,24 @@
+package faang.school.accountservice.controller;
+
+
+import faang.school.accountservice.dto.BalanceDto;
+import faang.school.accountservice.service.BalanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("balance/")
+public class BalanceController {
+
+    private final BalanceService balanceService;
+
+    @GetMapping("{balanceId}")
+    public BalanceDto getBalance(@PathVariable Long balanceId) {
+        return balanceService.getBalance(balanceId);
+    }
+
+}
