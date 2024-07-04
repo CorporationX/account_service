d9b000a462014f7b9fdb2d200187e6551388e5d4
ethiqque <danielpyld.work@gmail.com>
2024-07-04 08:31:40 +0300

    BJS2-11688

--- src/main/java/faang/school/accountservice/publisher/MessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package faang.school.accountservice.publisher;
-
-import faang.school.accountservice.event.Event;
-
-public interface MessagePublisher<T extends Event> {
-    void publish(T event);
-}
