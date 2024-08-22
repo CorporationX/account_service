60085b49b09eacdca8b6666757b7a60a4acf41ca
Mikhail Fokin <fokinms@gmail.com>
2024-08-22 11:40:58 +0400

    Change Lock type

--- src/main/java/faang/school/accountservice/repository/AccountRepository.java
+++ src/main/java/faang/school/accountservice/repository/AccountRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @Lock(LockModeType.OPTIMISTIC)
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     Optional<Account> findWithLockingById(BigInteger id);
 }
