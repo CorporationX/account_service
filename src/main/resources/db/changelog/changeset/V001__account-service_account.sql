1a98d719931f8540e8203807b6a9f631c17dd708
Max.Shundrik <mas9l175@gmail.com>
2024-08-30 08:41:23 +0300

    started with bjs2-25853

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,28 @@
--- Write your sql migration here!
+create table account_type
+(
+    id   bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    name varchar(128) not null
+);
+
+insert into account_type (name)
+values ('credit account'),
+       ('settlement account'),
+       ('fixed-term savings account'),
+       ('salary account'),
+       ('corporate account');
+
+create table account_numbers_sequence
+(
+    id           bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number       bigint not null,
+    account_type_id bigint not null,
+    constraint account_type_fk foreign key (account_type_id) references account_type (id) ON DELETE CASCADE
+);
+
+create table free_accounts_numbers
+(
+    id           bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    free_number  bigint not null,
+    account_type_id bigint not null,
+    constraint account_type_fk foreign key (account_type_id) references account_type (id) ON DELETE CASCADE
+);
