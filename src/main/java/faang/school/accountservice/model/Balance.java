7603b07d324817efa5e189b9de9953296edc8c07
grisatkac <grisatkac@mail.ru>
2024-09-17 23:49:00 +0300

    update equal and hash code

--- src/main/java/faang/school/accountservice/model/Balance.java
+++ src/main/java/faang/school/accountservice/model/Balance.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -16,19 +15,17 @@ import jakarta.persistence.TemporalType;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -37,6 +34,7 @@ import java.util.UUID;
 @Setter
 @ToString
 @AllArgsConstructor
+@EqualsAndHashCode
 @RequiredArgsConstructor
 @Table(name = "balance")
 public class Balance {
@@ -73,17 +71,4 @@ public class Balance {
     @OneToOne
     @JoinColumn(name = "account_id")
     private Account account;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Balance balance = (Balance) o;
-        return id != null && Objects.equals(id, balance.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
