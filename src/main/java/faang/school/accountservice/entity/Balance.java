db2f60a94421fa92e41c470b2aa9a76f87cef584
ifef228 <ifef228@gmail.com>
2024-10-21 18:21:48 +0300

    методы контроллера и сервиса

--- src/main/java/faang/school/accountservice/entity/Balance.java
+++ src/main/java/faang/school/accountservice/entity/Balance.java
@@ -8,10 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 @Table(name = "balance")
 public class Balance {
     @Id
--- src/main/java/faang/school/accountservice/service/BalanceService.java
+++ src/main/java/faang/school/accountservice/service/BalanceService.java
@@ -1,24 +1,49 @@
 package faang.school.accountservice.service;
 
 import faang.school.accountservice.dto.BalanceDto;
+import faang.school.accountservice.entity.Balance;
+import faang.school.accountservice.mapper.BalanceMapper;
+import faang.school.accountservice.repository.AccountRepository;
 import faang.school.accountservice.repository.BalanceJpaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class BalanceService {
-    private final BalanceJpaRepository repository;
+    private final BalanceJpaRepository balanceRepository;
+    private final AccountRepository accountRepository;
+    private final BalanceMapper mapper;
 
     public void create(BalanceDto balanceDto) {
+        Balance balance = mapper.toEntity(balanceDto);
+        LocalDateTime now = LocalDateTime.now();
 
+        balance.setCreatedAt(now);
+        balance.setUpdatedAt(now);
+
+        balanceRepository.save(balance);
     }
 
     public void update(BalanceDto balanceDto) {
+        Balance balance = mapper.toEntity(balanceDto);
 
+        balance.setUpdatedAt(LocalDateTime.now());
+
+        balanceRepository.save(balance);
     }
 
-    public double getBalance(long accountId) {
+    public BalanceDto getBalance(long accountId) {
+        checkIsAccountExist(accountId);
+        return mapper.toDto(balanceRepository.findBalanceByAccount_Id(accountId));
+    }
 
+    private void checkIsAccountExist(long accountId) {
+        if (!accountRepository.existsById(accountId)) {
+            throw new EntityNotFoundException("Not found account with id = " + accountId);
+        }
     }
 }
