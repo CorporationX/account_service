22219e86d006587ea8da6ec1f4f068bdef7e8a44
ifef228 <ifef228@gmail.com>
2024-10-21 16:50:27 +0300

    миграция

--- src/main/resources/db/changelog/changeset/V002__create-balance.sql
+++ src/main/resources/db/changelog/changeset/V002__create-balance.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS balance(
     id bigint PRIMARY KEY,
+    account_id BIGSERIAL,
     cur_auth_balance int,
     cur_fact_balance int,
     created_at date,
     updated_at date,
+    constraint bank_account
+    foreign key(account_id) references account(id),
     version int NOT NULL
 )
