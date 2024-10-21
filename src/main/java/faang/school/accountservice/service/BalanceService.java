ee29257b75c251be3d1cb46a6c0a2f0dc5b65610
ifef228 <ifef228@gmail.com>
2024-10-21 17:25:57 +0300

    сервис

--- src/main/java/faang/school/accountservice/service/BalanceService.java
+++ src/main/java/faang/school/accountservice/service/BalanceService.java
@@ -1,9 +1,24 @@
 package faang.school.accountservice.service;
 
+import faang.school.accountservice.dto.BalanceDto;
+import faang.school.accountservice.repository.BalanceJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BalanceService {
+    private final BalanceJpaRepository repository;
+
+    public void create(BalanceDto balanceDto) {
+
+    }
+
+    public void update(BalanceDto balanceDto) {
+
+    }
+
+    public double getBalance(long accountId) {
+
+    }
 }
