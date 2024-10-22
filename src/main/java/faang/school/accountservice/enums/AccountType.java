5796cfcb3008d1a742a195f1dec2d21390c4a196
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-22 12:17:48 +0300

    Внес правки

--- src/main/java/faang/school/accountservice/enums/AccountType.java
+++ src/main/java/faang/school/accountservice/enums/AccountType.java
@@ -6,11 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AccountType {
-    CHECKING_INDIVIDUAL(4200),
-    CHECKING_CORPORATE(4210),
-    CURRENCY_ACCOUNT(4300),
-    SAVINGS_ACCOUNT(5236),
-    INVESTMENT_ACCOUNT(5400);
+    CHECKING_INDIVIDUAL(4200, "checking_individual_seq"),
+    CHECKING_CORPORATE(4210, "checking_corporate_seq"),
+    CURRENCY_ACCOUNT(4300, "currency_account_seq"),
+    SAVINGS_ACCOUNT(5236, "savings_account_seq"),
+    INVESTMENT_ACCOUNT(5400, "investment_account_seq");
 
     private final int number;
+    private final String sequenceName;
 }
