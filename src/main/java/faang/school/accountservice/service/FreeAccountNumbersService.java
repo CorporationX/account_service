9e6a60897813c6f8a8f0919b88eb3a79ac6e40ae
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-22 19:00:20 +0300

    Решение

---
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
@@ -0,0 +1,15 @@
+package faang.school.accountservice.service;
+
+import faang.school.accountservice.entity.FreeAccountNumber;
+import faang.school.accountservice.enums.AccountType;
+
+import java.util.function.Consumer;
+
+public interface FreeAccountNumbersService {
+
+    String generateNumberByType(AccountType type);
+
+    void generateNumberAndExecute(AccountType type, Consumer<FreeAccountNumber> operation);
+
+    void saveNumber(AccountType type, String accountNumber);
+}
---
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersServiceImpl.java
@@ -0,0 +1,53 @@
+package faang.school.accountservice.service;
+
+import faang.school.accountservice.entity.FreeAccountNumber;
+import faang.school.accountservice.entity.FreeAccountNumberId;
+import faang.school.accountservice.enums.AccountType;
+import faang.school.accountservice.repository.AccountNumbersSequenceRepository;
+import faang.school.accountservice.repository.FreeAccountNumbersRepository;
+import faang.school.accountservice.util.AccountNumberGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Consumer;
+
+@Service
+@RequiredArgsConstructor
+public class FreeAccountNumbersServiceImpl implements FreeAccountNumbersService {
+
+    private final FreeAccountNumbersRepository freeAccountNumbersRepository;
+    private final AccountNumbersSequenceRepository accountNumbersSequenceRepository;
+    private final AccountNumberGenerator accountNumberGenerator;
+
+    @Override
+    public String generateNumberByType(AccountType type) {
+        return freeAccountNumbersRepository.getFreeAccountNumberByType(type.name())
+                .orElseGet(() -> {
+                    Long number = accountNumbersSequenceRepository.incrementAndGet(type.name());
+                    return accountNumberGenerator.generateAccountNumber(type, number);
+                });
+    }
+
+    @Override
+    @Transactional
+    public void generateNumberAndExecute(AccountType type, Consumer<FreeAccountNumber> operation) {
+        String accountNumber = generateNumberByType(type);
+        FreeAccountNumberId id = FreeAccountNumberId.builder()
+                .accountNumber(accountNumber)
+                .type(type)
+                .build();
+        FreeAccountNumber accountNumberEntity = FreeAccountNumber.builder()
+                .id(id)
+                .build();
+        operation.accept(accountNumberEntity);
+    }
+
+    @Override
+    public void saveNumber(AccountType type, String accountNumber) {
+        FreeAccountNumber freeAccountNumber = FreeAccountNumber.builder()
+                .id(new FreeAccountNumberId(type, accountNumber))
+                .build();
+        freeAccountNumbersRepository.save(freeAccountNumber);
+    }
+}
