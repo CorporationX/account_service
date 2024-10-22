a42a84b5ca848207fb5ab40e143a09b5834032ae
Denis Artamonov <dns.artamonov@gmail.com>
2024-10-22 12:34:24 +0300

    BJS2-27336

--- src/main/java/faang/school/accountservice/service/impl/AccountServiceImpl.java
+++ src/main/java/faang/school/accountservice/service/impl/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import faang.school.accountservice.model.entity.Account;
 import faang.school.accountservice.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,16 +14,19 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
 
+    @Transactional(readOnly = true)
     @Override
     public AccountDto getAccount(Long id) {
         Account account = accountRepository.findById(id).orElse(null);
+
         return accountMapper.accountToAccountDto(account);
     }
 
+    @Transactional
     @Override
     public AccountDto openAccount(AccountDto accountDto) {
         Account account = accountMapper.accountDtoToAccount(accountDto);
-
-        return null;
+        account.setNumber(null);
+        return accountMapper.accountToAccountDto(accountRepository.save(account));
     }
 }
