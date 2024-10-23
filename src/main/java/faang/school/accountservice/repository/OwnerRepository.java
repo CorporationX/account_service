f077e594319b1cbc905bf5c540681e5722362aab
Peregruzochka <peregruzochka48@mail.ru>
2024-10-23 12:20:55 +0300

    Add AccountService

---
+++ src/main/java/faang/school/accountservice/repository/OwnerRepository.java
@@ -0,0 +1,20 @@
+package faang.school.accountservice.repository;
+
+import faang.school.accountservice.model.owner.Owner;
+import faang.school.accountservice.model.owner.OwnerType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+    @Query("""
+            SELECT o FROM Owner o
+            WHERE o.externalId = :externalId AND o.type = :type
+            """)
+    Optional<Owner> findOwner(Long externalId, OwnerType type);
+
+}
--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,24 @@
--- Write your sql migration here!
+CREATE TABLE payment_owners (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    external_id BIGINT NOT NULL,
+    type VARCHAR(32) NOT NULL,
+    created_at timestamptz DEFAULT current_timestamp NOT NULL
+);
+
+CREATE TABLE payment_accounts (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    account_number VARCHAR(20) NOT NULL UNIQUE,
+    owner_id bigint NOT NULL,
+    type VARCHAR(32) NOT NULL,
+    currency VARCHAR(8) NOT NULL,
+    status VARCHAR(32) DEFAULT 'ACTIVE' NOT NULL,
+    created_at timestamptz DEFAULT current_timestamp NOT NULL,
+    updated_at timestamptz DEFAULT current_timestamp NOT NULL,
+    closed_at timestamptz,
+    version bigint DEFAULT 0 NOT NULL,
+
+    CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES payment_owners(id)
+);
+
+CREATE INDEX idx_payment_owners_external_type ON payment_owners (external_id, type);
+CREATE INDEX idx_account_number ON payment_accounts(account_number);
