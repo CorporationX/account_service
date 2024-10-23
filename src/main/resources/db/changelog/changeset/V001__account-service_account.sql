602035e483fda4eaf2fe4788cd43e290d8517899
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-10-24 03:10:35 +0700

    resolve remarks

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE IF NOT EXISTS accounts_owners_id_seq START WITH 1 INCREMENT BY 1
 CREATE TABLE IF NOT EXISTS public.accounts_owners(
     id bigint PRIMARY KEY DEFAULT nextval('accounts_owners_id_seq'),
     owner_id bigint NOT NULL,
-    owner_type varchar(128) NOT NULL,
+    owner_type smallint NOT NULL,
     UNIQUE(owner_id, owner_type)
 );
 
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS public.accounts(
     id bigint PRIMARY KEY DEFAULT nextval('accounts_id_seq'),
     account_number varchar(20) NOT NULL UNIQUE CHECK(length(account_number) >= 12),
     owner_id bigint NOT NULL,
-    account_type varchar(128) NOT NULL,
-    currency character(3) NOT NULL,
-    status varchar(128) NOT NULL DEFAULT 0,
+    account_type smallint NOT NULL,
+    currency smallint NOT NULL,
+    status smallint NOT NULL DEFAULT 0,
     closed_at timestamptz,
     version bigint NOT NULL DEFAULT 0,
     created_at timestamptz DEFAULT current_timestamp,
