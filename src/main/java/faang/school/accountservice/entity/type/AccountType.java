26d3c34618192509452ae9a989a66115cf79e70e
pgusev <pgusev@fil-it.ru>
2024-10-23 23:15:16 +0300

    BJS2-26200 - Created base logic for payment account

---
+++ src/main/java/faang/school/accountservice/entity/type/AccountType.java
@@ -0,0 +1,45 @@
+package faang.school.accountservice.entity.type;
+
+import faang.school.accountservice.entity.account.Account;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "type")
+public class AccountType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", length = 24, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "type")
+    private List<Account> accounts;
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
