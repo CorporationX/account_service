e2945f5c274e4a56517d723b3b70abe692601e62
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-23 20:43:22 +0300

    сделал миграцию

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,17 @@
--- Write your sql migration here!
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    number VARCHAR(20) NOT NULL CHECK (LENGTH(number) BETWEEN 12 AND 20) UNIQUE,
+    project_id BIGINT NOT NULL UNIQUE,
+    user_id BIGINT NOT NULL UNIQUE,
+    account_type VARCHAR(32) NOT NULL,
+    currency VARCHAR(32) NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    changed_at TIMESTAMP NOT NULL,
+    closed_at TIMESTAMP,
+    version BIGINT NOT NULL DEFAULT 1
+);
+CREATE INDEX project_id_index ON account (project_id);
+CREATE INDEX user_id_index ON account (user_id);
+
+CHECK ((project_id IS NOT NULL AND user_id IS NULL) OR (project_id IS NULL AND user_id IS NOT NULL))
