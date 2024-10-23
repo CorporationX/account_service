146db60edf3984817a517f1923ad015a1ae60b33
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-23 22:35:16 +0300

    сделал миграцию

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -9,9 +9,9 @@ CREATE TABLE account (
     created_at TIMESTAMP NOT NULL,
     changed_at TIMESTAMP NOT NULL,
     closed_at TIMESTAMP,
-    version BIGINT NOT NULL DEFAULT 1
+    version BIGINT NOT NULL DEFAULT 1,
+    CONSTRAINT one_owner_id_check CHECK ((project_id IS NOT NULL AND user_id IS NULL) OR (project_id IS NULL AND user_id IS NOT NULL))
 );
 CREATE INDEX project_id_index ON account (project_id);
 CREATE INDEX user_id_index ON account (user_id);
 
-CHECK ((project_id IS NOT NULL AND user_id IS NULL) OR (project_id IS NULL AND user_id IS NOT NULL))
