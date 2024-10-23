e24078f48d238b9790311a803e9abbabb288fe60
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-10-23 20:48:13 +0300

    added account table and index migration

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,18 @@
--- Write your sql migration here!
+create table if not exists account
+(
+    id             bigserial primary key,
+    account_number varchar(20) not null check ( length(account_number) between 12 and 20),
+    owner_id       bigint      not null,
+    owner_type     smallint    not null,
+    account_type   smallint    not null,
+    currency       smallint    not null,
+    account_status smallint    not null,
+    created_at     timestamp            default current_timestamp,
+    updated_at     timestamp            default current_timestamp,
+    closed_at      timestamp,
+    version        bigint      not null default 1,
+    unique (account_number)
+);
+
+create index idx_account_number on account (account_number);
+create index idx_account_number on account (owner_id, owner_type);
