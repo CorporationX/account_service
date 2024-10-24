7c4b71238556f8e677727cd8140573b19cbba75b
Arstan Subanov <arstansubanov@gmail.com>
2024-10-24 21:44:17 +0600

    migration: add balance

---
+++ src/main/java/faang/school/accountservice/model/Balance.java
@@ -0,0 +1,59 @@
+package faang.school.accountservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "balance")
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "auth_balance", nullable = false)
+    private BigDecimal authBalance;
+
+    @Column(name = "actual_balance", nullable = false)
+    private BigDecimal actualBalance;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Version
+    @Column(name = "version")
+    private Long version;
+}
---
+++ src/main/resources/db/changelog/changeset/account_V002__create_balance.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS balance
+(
+    id             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id     BIGINT         NOT NULL REFERENCES accounts (id),
+    auth_balance   NUMERIC(15, 2) NOT NULL,
+    actual_balance NUMERIC(15, 2) NOT NULL,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    version        BIGINT    DEFAULT 0
+);
