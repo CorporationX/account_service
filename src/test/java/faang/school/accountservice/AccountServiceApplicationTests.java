090ad47745e07c3110207bf9bc4288edfee1c16c
Nikita Korolev <nk25.dev@gmail.com>
2024-10-24 16:01:06 +0300

    ContextLoads added

--- src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package faang.school.accountservice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class AccountServiceApplicationTests {
     @Test
-    void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
-    }
+    void contextLoads() {}
 }
