6188d534fe5312a1109b299b0629dc92fc66fc56
Denis Artamonov <dns.artamonov@gmail.com>
2024-10-25 14:08:44 +0300

    BJS2-27337

---
+++ src/main/java/faang/school/accountservice/model/entity/Tariff.java
@@ -0,0 +1,15 @@
+package faang.school.accountservice.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Entity
+@Table(name = "tariff")
+public class Tariff {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
