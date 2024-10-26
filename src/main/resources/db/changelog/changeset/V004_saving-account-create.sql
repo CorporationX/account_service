a9759e64fda43c5cb9b8d98b9c638464b067b666
Denis Artamonov <dns.artamonov@gmail.com>
2024-10-26 18:10:08 +0300

    BJS2-27337

--- src/main/resources/db/changelog/changeset/V004_saving-account-create.sql
+++ src/main/resources/db/changelog/changeset/V004_saving-account-create.sql
@@ -6,25 +6,28 @@ CREATE TABLE IF NOT EXISTS tariff_rate (
 CREATE TABLE IF NOT EXISTS tariff (
     id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     tariff_type VARCHAR(64) NOT NULL,
-    history_tariff_rate     int NOT NULL,
+    tariff_rate_id     int NOT NULL,
 
-    CONSTRAINT fk_tariff_rate_id FOREIGN KEY (history_tariff_rate) REFERENCES tariff_rate (id)
+    CONSTRAINT fk_tariff_rate_id FOREIGN KEY (tariff_rate_id) REFERENCES tariff_rate (id)
 );
 
 CREATE TABLE IF NOT EXISTS tariff_history (
-    id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,˚
+    tariff_id   bigint NOT NULL,
+
+    CONSTRAINT fk_tariff_id FOREIGN KEY (tariff_id) references tariff (id)
 );
 
-CREATE TABLE savings_account
+CREATE TABLE IF NOT EXISTS savings_account
 (
     id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     number     bigint UNIQUE NOT NULL,
-    tariff_history bigint NOT NULL,
+    tariff_history_id bigint NOT NULL,
     last_date_percent    timestamptz,
     version    bigint                      DEFAULT 1,
     created_at timestamptz                 DEFAULT current_timestamp,
     updated_at timestamptz                 DEFAULT current_timestamp,
 
     CONSTRAINT fk_account_id FOREIGN KEY (number) REFERENCES account (id),
-    CONSTRAINT fk_tariff_history_id FOREIGN KEY (tariff_history) REFERENCES tariff (id)
+    CONSTRAINT fk_tariff_history_id FOREIGN KEY (tariff_history_id) REFERENCES tariff_history (id)
 );
