00767f60ba7349514e608c7de7210b6a31486d79
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-10-26 14:44:15 +0300

    BJS2-27338 - реализована генерация уникальных номеров счетов

---
+++ src/main/java/faang/school/accountservice/scheduler/FreeNumbersGenerationScheduler.java
@@ -0,0 +1,32 @@
+package faang.school.accountservice.scheduler;
+
+import faang.school.accountservice.config.scheduler.ScheduledAccountConfig;
+import faang.school.accountservice.model.enums.AccountType;
+import faang.school.accountservice.service.FreeAccountNumbersService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FreeNumbersGenerationScheduler {
+    private final ScheduledAccountConfig scheduledAccountConfig;
+    private final FreeAccountNumbersService freeAccountNumbersService;
+
+    @Scheduled(cron = "${scheduler.account-numbers-generation}")
+    public void generateAccountNumbersOnSchedule() {
+        for (AccountType accountType : AccountType.values()) {
+            Integer targetAmount = getTargetAmountForAccountType(accountType);
+            if (targetAmount != null) {
+              freeAccountNumbersService.ensureMinimumAccountNumbers(accountType, targetAmount);
+            }
+        }
+    }
+
+    private Integer getTargetAmountForAccountType(AccountType accountType) {
+        ScheduledAccountConfig.AccountConfig config = scheduledAccountConfig.getAccounts().get(accountType.name());
+        return config != null ? config.getTargetAmount() : null;
+    }
+}
