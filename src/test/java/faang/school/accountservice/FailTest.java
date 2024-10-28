33cafc731cdb2ba50efe0a168a40e22818d824de
ifef228 <ifef228@gmail.com>
2024-10-28 09:36:19 +0300

    ci

---
+++ src/test/java/faang/school/accountservice/FailTest.java
@@ -0,0 +1,11 @@
+package faang.school.accountservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FailTest {
+    @Test
+    void failTestMethod() {
+        Assertions.fail();
+    }
+}
