5ddad3292b03c1438ccebbbafa9b53845e68af48
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-10-28 19:19:55 +0300

    RedisConfig и Listener для DMS

---
+++ src/main/java/faang/school/accountservice/config/redis/RedisConfig.java
@@ -0,0 +1,45 @@
+package faang.school.accountservice.config.redis;
+
+import faang.school.accountservice.publis.listener.PaymentEventListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.Topic;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+    private final PaymentEventListener paymentEventListener;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer redisContainer = new RedisMessageListenerContainer();
+        redisContainer.setConnectionFactory(redisConnectionFactory());
+
+        redisContainer.addMessageListener(paymentListenerAdapter(), paymentTopic());
+
+        return redisContainer;
+    }
+
+    @Bean
+    public MessageListener paymentListenerAdapter() {
+        return new MessageListenerAdapter(paymentEventListener);
+    }
+
+    @Bean
+    public Topic paymentTopic() {
+        return new ChannelTopic(redisProperties.getPaymentEventChannelName());
+    }
+}
---
+++ src/main/java/faang/school/accountservice/publis/listener/PaymentEventListener.java
@@ -0,0 +1,30 @@
+package faang.school.accountservice.publis.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.accountservice.dto.payment.PaymentDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentEventListener implements MessageListener {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String messageBody = new String(message.getBody());
+        log.info("Get message: {}", messageBody);
+        try {
+            objectMapper.readValue(messageBody, PaymentDto.class);
+        } catch (IOException exception) {
+            log.error(exception.getMessage(), exception);
+            throw new IllegalArgumentException(exception);
+        }
+    }
+}
