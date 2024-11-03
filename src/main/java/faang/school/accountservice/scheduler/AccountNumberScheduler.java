6bb1ff44f1aa44c8e5512a816390fe2185e513ba
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-11-04 00:54:48 +0300

    разделил сервис на интерфейс и реализацию

--- src/main/java/faang/school/accountservice/scheduler/AccountNumberScheduler.java
+++ src/main/java/faang/school/accountservice/scheduler/AccountNumberScheduler.java
@@ -1,7 +1,7 @@
 package faang.school.accountservice.scheduler;
 
 import faang.school.accountservice.enums.AccountType;
-import faang.school.accountservice.service.FreeAccountNumbersService;
+import faang.school.accountservice.service.FreeAccountNumbersServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,7 +14,7 @@ public class AccountNumberScheduler {
     @Value("${scheduler.batch-size}")
     private int batchSize;
 
-    private final FreeAccountNumbersService freeAccountNumbersService;
+    private final FreeAccountNumbersServiceImpl freeAccountNumbersService;
 
     @Scheduled(cron = "${scheduler.cron}")
     public void generateDebit() {
--- src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
@@ -2,45 +2,13 @@ package faang.school.accountservice.service;
 
 import faang.school.accountservice.entity.FreeAccountNumber;
 import faang.school.accountservice.enums.AccountType;
-import faang.school.accountservice.repository.AccountSeqRepository;
-import faang.school.accountservice.repository.FreeAccountNumberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
-@Service
-@RequiredArgsConstructor
-public class FreeAccountNumbersService {
+public interface FreeAccountNumbersService {
 
-    private static final long ACCOUNT_PATTERN = 4200_0000_0000_0000L;
+    void generateAccountNumbers(AccountType type, int batchSize);
 
-    private final FreeAccountNumberRepository freeAccountNumberRepository;
-    private final AccountSeqRepository accountSeqRepository;
-
-    @Transactional
-    public void generateAccountNumbers(AccountType type, int batchSize) {
-        long period = accountSeqRepository.findByAccountType(type.name()).getCounter();
-        accountSeqRepository.incrementCounter(type.name(), batchSize);
-        long updatedCount = accountSeqRepository.findByAccountType(type.name()).getCounter();
-
-        List<FreeAccountNumber> numbers = new ArrayList<>();
-        for (long i = period; i < updatedCount; i++) {
-            FreeAccountNumber accountNumber = new FreeAccountNumber();
-            accountNumber.setAccountType(type);
-            accountNumber.setAccountNumber(ACCOUNT_PATTERN + i);
-
-            numbers.add(accountNumber);
-        }
-        freeAccountNumberRepository.saveAll(numbers);
-    }
-
-    @Transactional
-    public void retrieveAccountNumber(AccountType type, Consumer<FreeAccountNumber> numberConsumer) {
-        numberConsumer.accept(freeAccountNumberRepository.retrieveFirst(type.name()));
-    }
+    void retrieveAccountNumber(AccountType type, Consumer<FreeAccountNumber> numberConsumer);
 
 }
