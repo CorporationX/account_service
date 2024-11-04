84ae9da0620ff8471b50afc88b81e21f383436b7
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-11-04 15:14:05 +0700

    resolve remarks

--- src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
@@ -2,7 +2,6 @@ package faang.school.accountservice;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class AccountServiceApplicationTests {
     @Test
