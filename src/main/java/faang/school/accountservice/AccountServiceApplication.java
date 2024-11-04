fe0d3bcd46522be6bd3b4644e0c1621d8f82daad
Pavel Gusev <64732451+GusevVrn@users.noreply.github.com>
2024-11-04 09:31:59 +0300

    BJS2-26200 - Swagger for account service (#276)
    BJS2-26200 - Swagger for account service

--- src/main/java/faang/school/accountservice/AccountServiceApplication.java
+++ src/main/java/faang/school/accountservice/AccountServiceApplication.java
@@ -1,11 +1,17 @@
 package faang.school.accountservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Account service api",
+                description = "Account service", version = "1.0.0"))
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("faang.school.accountservice.client")
--- src/main/java/faang/school/accountservice/controller/account/AccountController.java
+++ src/main/java/faang/school/accountservice/controller/account/AccountController.java
@@ -3,6 +3,9 @@ package faang.school.accountservice.controller.account;
 import faang.school.accountservice.dto.account.AccountCreateDto;
 import faang.school.accountservice.dto.account.AccountDto;
 import faang.school.accountservice.service.account.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,34 +26,54 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/accounts")
+@Tag(name = "Account controller", description = "Controller with accounts REST endpoints")
 public class AccountController {
 
     private final AccountService accountService;
 
+    @Operation(
+            summary = "Get all accounts"
+    )
     @GetMapping
     public List<AccountDto> getAccounts() {
         return accountService.getAccounts();
     }
 
+    @Operation(
+            summary = "Get account",
+            description = "Get account by ID"
+    )
     @GetMapping("/{id}")
-    public AccountDto getAccount(@PathVariable("id") long accountId) {
+    public AccountDto getAccount(@PathVariable("id") @Parameter(description = "account Id") long accountId) {
         return accountService.getAccountByAccountId(accountId);
     }
 
+    @Operation(
+            summary = "Open account",
+            description = "Generate new account"
+    )
     @PostMapping("/open")
     public AccountDto createAccount(@RequestBody @Valid AccountCreateDto accountCreateDto) {
         return accountService.createAccount(accountCreateDto);
     }
 
+    @Operation(
+            summary = "Block account",
+            description = "Set account status FREEZE by account id"
+    )
     @PatchMapping("/{id}/block")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public AccountDto blockAccount(@PathVariable("id") long accountId) {
+    public AccountDto blockAccount(@PathVariable("id") @Parameter(description = "account Id") long accountId) {
         return accountService.blockAccount(accountId);
     }
 
+    @Operation(
+            summary = "Delete account",
+            description = "Set account status CLOSED by account id"
+    )
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void closeAccount(@PathVariable("id") long accountId) {
+    public void closeAccount(@PathVariable("id") @Parameter(description = "account Id") long accountId) {
         accountService.closeAccount(accountId);
     }
 }
