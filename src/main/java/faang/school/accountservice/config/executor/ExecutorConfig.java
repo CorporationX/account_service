180563cb0e7b85fbb2a1e52756a3cbc58d19478f
Okabe <mathew.mackonoha@gmail.com>
2024-11-05 09:59:40 +0300

    Пишу Scheduler

--- src/main/java/faang/school/accountservice/config/executor/ExecutorConfig.java
+++ src/main/java/faang/school/accountservice/config/executor/ExecutorConfig.java
@@ -1,11 +1,56 @@
 package faang.school.accountservice.config.executor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
 public class ExecutorConfig {
     private final ExecutorProperties executorProperties;
-    // привинчиваю бины экзекьюторов и делаю их тиканье через хэндлеры
+
+    @Bean
+    public ThreadPoolExecutor amountHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    @Bean
+    public ThreadPoolExecutor accountRecordHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    @Bean
+    public ThreadPoolExecutor auditRecordHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    @Bean
+    public ThreadPoolExecutor balanceRecordHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    @Bean
+    public ThreadPoolExecutor openAccountNotificationHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    @Bean
+    public ThreadPoolExecutor cashbackRecordHandlerExecutor() {
+        return createThreadPoolExecutor();
+    }
+
+    private ThreadPoolExecutor createThreadPoolExecutor() {
+        return new ThreadPoolExecutor(
+                executorProperties.getCorePoolSize(),
+                executorProperties.getMaxPoolSize(),
+                executorProperties.getKeepAliveTime(),
+                TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(executorProperties.getQueueCapacity())
+        );
+    }
 }
+
