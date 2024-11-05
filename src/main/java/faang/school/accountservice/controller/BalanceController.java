bf11da2329c653aed88345da4f58ce1d70ce65f2
Lexus-74 <lexus__74@mail.ru>
2024-11-05 21:29:29 +0500

    Правки после ревью

--- src/main/java/faang/school/accountservice/controller/BalanceController.java
+++ src/main/java/faang/school/accountservice/controller/BalanceController.java
@@ -1,19 +1,12 @@
 package faang.school.accountservice.controller;
 
-import faang.school.accountservice.dto.Money;
 import faang.school.accountservice.mapper.BalanceMapper;
 import faang.school.accountservice.dto.balance.BalanceDto;
 import faang.school.accountservice.model.balance.Balance;
-import faang.school.accountservice.model.balance.BalanceAuthPayment;
 import faang.school.accountservice.service.BalanceService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,15 +24,5 @@ public class BalanceController {
     public BalanceDto getBalance(@RequestParam @NotNull UUID accountUuid) {
         Balance balance = balanceService.getBalance(accountUuid);
         return balanceMapper.toDto(balance);
-    }
-
-    @PostMapping("/{balanceId}")
-    public void createBalance(@Valid @PathVariable UUID balanceId, @Valid @RequestBody Money money) {
-        BalanceAuthPayment balanceAuthPayment = balanceService.createAuthPayment(balanceId, money);
-    }
-
-    @PutMapping("/{balanceAuthId}")
-    public void rejectAuthPayment(@Valid @PathVariable UUID balanceAuthId) {
-        BalanceAuthPayment balanceAuthPayment = balanceService.rejectAuthPayment(balanceAuthId);
     }
 }
