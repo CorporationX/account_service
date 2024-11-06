27cf8908ccf0f7dfa48d807e432ab185b67a9059
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-11-06 14:41:48 +0300

    feature-BJS2-26201 / Работа с балансами /AccountService

--- src/main/java/faang/school/accountservice/controller/balance/BalanceController.java
+++ src/main/java/faang/school/accountservice/controller/balance/BalanceController.java
@@ -1,4 +1,4 @@
-package faang.school.accountservice.controller.account.balance;
+package faang.school.accountservice.controller.balance;
 
 import faang.school.accountservice.dto.balance.BalanceDto;
 import faang.school.accountservice.dto.balance.TransactionDto;
