d2770168d9753f4801eeb19a55957543d26f91bd
Sergey Sklyar <sklyar1091@gmail.com>
2024-11-09 01:15:17 +0300

    - unused imports

--- src/main/java/faang/school/accountservice/repository/account/FreeAccountNumbersRepository.java
+++ src/main/java/faang/school/accountservice/repository/account/FreeAccountNumbersRepository.java
@@ -1,19 +1,14 @@
 package faang.school.accountservice.repository.account;
 
-import java.util.List;
-import java.util.Optional;
-
+import faang.school.accountservice.entity.account.FreeAccountId;
+import faang.school.accountservice.entity.account.FreeAccountNumber;
 import faang.school.accountservice.enums.account.AccountEnum;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import faang.school.accountservice.entity.account.FreeAccountId;
-import faang.school.accountservice.entity.account.FreeAccountNumber;
+import java.util.Optional;
 
 @Repository
 public interface FreeAccountNumbersRepository extends JpaRepository<FreeAccountNumber, FreeAccountId> ,
