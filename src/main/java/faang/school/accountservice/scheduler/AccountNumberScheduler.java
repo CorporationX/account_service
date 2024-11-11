31ed98b8d19c354982933c1d4625d6af77f70907
vestmen <alex.nemtsev95@gmail.com>
2024-11-11 09:20:00 +0300

    BJS2-27150 Генерация уникальных номеров счетов правки после слияния и фикс багов

--- src/main/java/faang/school/accountservice/scheduler/AccountNumberScheduler.java
+++ src/main/java/faang/school/accountservice/scheduler/AccountNumberScheduler.java
@@ -11,8 +11,8 @@ import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Component
 @RequiredArgsConstructor
+@Component
 public class AccountNumberScheduler {
     private final FreeAccountNumberService freeAccountNumberService;
     private final AccountType[] accountTypes = AccountType.values();
