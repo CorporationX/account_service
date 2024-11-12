e2bd61ae2745f0affb1c8eb829b3be8d945dae4b
PLSGOTOASS <koteykaarisha@mail.ru>
2024-11-12 05:12:41 +0300

    поправил

--- src/main/java/faang/school/accountservice/repository/MerchantRepository.java
+++ src/main/java/faang/school/accountservice/repository/MerchantRepository.java
@@ -2,6 +2,8 @@ package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.type.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 }
--- src/main/java/faang/school/accountservice/repository/OperationTypeRepository.java
+++ src/main/java/faang/school/accountservice/repository/OperationTypeRepository.java
@@ -2,6 +2,8 @@ package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.type.OperationType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OperationTypeRepository extends JpaRepository<OperationType, Long> {
 }
