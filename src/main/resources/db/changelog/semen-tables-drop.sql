11131a9ed7bfdfcbba5387d6887f5096b357f1c1
Sultan <sultan.inked@gmail.com>
2024-11-14 15:08:51 +0500

    create drop table

---
+++ src/main/resources/db/changelog/semen-tables-drop.sql
@@ -0,0 +1,11 @@
+drop INDEX idx_auth_payment_status_id_created_at;
+ALTER TABLE account drop CONSTRAINT fk_account_cashback_tariff;
+ALTER TABLE account drop COLUMN cashback_tariff_id;
+drop INDEX idx_cashback_merchant_cashback_tariff_id_merchant_id;
+drop TABLE cashback_merchant;
+drop TABLE cashback_operation_type ;
+drop INDEX uniq_merchant_user_id;
+drop INDEX uniq_merchant_project_id;
+drop TABLE merchant;
+drop INDEX uniq_cashback_tariff_name;
+drop TABLE cashback_tariff;
