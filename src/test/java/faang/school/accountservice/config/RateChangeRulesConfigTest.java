4956da7e10cf9b00ed7bffe91f07144fe6b05fe6
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-11-15 17:29:03 +0300

    checking for Mock DataSource

--- src/test/java/faang/school/accountservice/config/RateChangeRulesConfigTest.java
+++ src/test/java/faang/school/accountservice/config/RateChangeRulesConfigTest.java
@@ -1,10 +1,12 @@
 package faang.school.accountservice.config;
 
 import faang.school.accountservice.config.ratechange.RateChangeRulesConfig;
+import jakarta.activation.DataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -18,6 +20,9 @@ public class RateChangeRulesConfigTest {
 
     @Autowired
     private RateChangeRulesConfig rateChangeRulesConfig;
+
+    @MockBean
+    private DataSource dataSource;
 
     @Test
     @DisplayName("Should load 'writer' rate correctly from configuration")
