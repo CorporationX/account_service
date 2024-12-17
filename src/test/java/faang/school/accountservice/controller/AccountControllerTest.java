dc150ef1ee0a372a2d6288068544d398f2714dd7
SiarheiHaurylchuk <sggavrilchik@gmail.com>
2024-12-17 22:30:34 +0300

    fix4

--- src/test/java/faang/school/accountservice/controller/AccountControllerTest.java
+++ src/test/java/faang/school/accountservice/controller/AccountControllerTest.java
@@ -130,6 +130,7 @@ public class AccountControllerTest {
         return AccountDto.builder()
                 .id(1L)
                 .number("1234567890")
+                .accountType("PERSONAL")
                 .currency(Currency.USD)
                 .status(AccountStatus.ACTIVE)
                 .version("1.0")
