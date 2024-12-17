dfbe1b3739b02a4b98972f59c91e30cd9aa16429
SiarheiHaurylchuk <sggavrilchik@gmail.com>
2024-12-17 22:11:03 +0300

    test

---
+++ src/test/java/faang/school/accountservice/TestCi/CITEST.java
@@ -0,0 +1,13 @@
+package faang.school.accountservice.TestCi;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CITEST {
+    @Test
+    void test() {
+        fail();
+    }
+}
