c68f4cd4df0655bbf5962ea6adc4fd30e3ec2021
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-12-18 16:55:08 +0300

    Добавил аннотации @NotNull в ДТО

--- src/main/java/faang/school/accountservice/dto/BalanceDto.java
+++ src/main/java/faang/school/accountservice/dto/BalanceDto.java
@@ -1,5 +1,6 @@
 package faang.school.accountservice.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,9 +10,12 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class BalanceDto {
+    @NotNull
     private Long id;
     private Long accountId;
+    @NotNull
     private BigDecimal authBalance;
+    @NotNull
     private BigDecimal actualBalance;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
