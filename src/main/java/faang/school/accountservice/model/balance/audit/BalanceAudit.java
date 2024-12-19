36dcb3c3b284ebadb0fa32384e4bac3d4dc86b15
Maks Russkikh <15maksim@mail.ru>
2024-12-19 11:30:04 +0300

    create table and entity

---
+++ src/main/java/faang/school/accountservice/model/balance/audit/BalanceAudit.java
@@ -0,0 +1,40 @@
+package faang.school.accountservice.model.balance.audit;
+
+import faang.school.accountservice.model.account.Account;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "balance_audit")
+public class BalanceAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "version")
+    private int version;
+
+    @Column(name = "authorization_amount", nullable = false)
+    private Long authorizationAmount;
+
+    @Column(name = "actual_amount", nullable = false)
+    private Long actualAmount;
+
+    @Column(name = "id_operation", nullable = false)
+    private long id_operation;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+}
