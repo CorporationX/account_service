685f32cc20a7b6fe24f7747fb321db48f1d20221
Denis Levkhov <levkhovde@yandex.ru>
2024-12-21 23:06:18 +0300

    BJS2-41945: Add trigger for balance update

--- src/main/resources/db/changelog/changeset/V003_account-service_added_balance_table.sql
+++ src/main/resources/db/changelog/changeset/V003_account-service_added_balance_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE balance
 (
-    id                 BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
-    account_id         BIGINT         NOT NULL REFERENCES account (id) ON DELETE RESTRICT,
+    id                 BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id         BIGINT         NOT NULL REFERENCES account (id) ON DELETE CASCADE,
     authorized_balance NUMERIC(18, 2) NOT NULL DEFAULT 0.00,
     actual_balance     NUMERIC(18, 2) NOT NULL DEFAULT 0.00,
     created_at         TIMESTAMPTZ    NOT NULL DEFAULT CURRENT_TIMESTAMP,
