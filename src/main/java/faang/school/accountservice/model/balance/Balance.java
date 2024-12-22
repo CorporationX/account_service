1cff3727b6aed468e092e9d9ae22c562204c21ed
Igor <asachiyigor@gmail.com>
2024-12-22 14:59:33 +0300

    Задача "feature-BJS2-41933-Igor" BJS2-41933
    add balance

---
+++ src/main/java/faang/school/accountservice/model/balance/Balance.java
@@ -0,0 +1,59 @@
+package faang.school.accountservice.model.balance;
+
+import faang.school.accountservice.model.account.Account;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "balance")
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    private Account account;
+
+    @Column(name = "authorized_value")
+    private BigDecimal authorizedValue;
+
+    @Column(name = "actual_value")
+    private BigDecimal actualValue;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "version", nullable = false)
+    @Version
+    private long version;
+}
