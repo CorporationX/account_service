652bc5010bec58126ebc9bdd523d8f6b3f8f4c85
SerUKalinin <serukalinin@gmail.com>
2024-12-22 09:27:23 +0300

    Изменение названия столбцов, так как поменяли account_type на accounttype

--- src/main/resources/db/changelog/changeset/V002__create_free_account_numbers.sql
+++ src/main/resources/db/changelog/changeset/V002__create_free_account_numbers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.free_account_numbers (
     id BIGSERIAL PRIMARY KEY,
-    account_type VARCHAR(4) NOT NULL,
+    accounttype VARCHAR(4) NOT NULL,
     account_number VARCHAR(20) NOT NULL UNIQUE,
     CONSTRAINT unique_account_type_number
-        UNIQUE (account_type, account_number)
+        UNIQUE (accounttype, account_number)
 );
--- src/main/resources/db/changelog/changeset/V004__create_account_numbers_sequence.sql
+++ src/main/resources/db/changelog/changeset/V004__create_account_numbers_sequence.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.account_numbers_sequence (
     id BIGSERIAL PRIMARY KEY,
-    account_type VARCHAR(4) NOT NULL UNIQUE,
+    accounttype VARCHAR(4) NOT NULL UNIQUE,
     current_number BIGINT NOT NULL DEFAULT 0
 );
