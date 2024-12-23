7bfeb2ea8b7ba925e8eb744cd58f203c9fbb4712
Andrew <andrew@MPro.local>
2024-12-24 00:12:42 +0700

    Feature / Account Service / Adds Account Balance
    Adds AccountBalanceDto and TransactionDto

---
+++ src/main/java/faang/school/accountservice/dto/AccountBalanceDto.java
@@ -0,0 +1,11 @@
+package faang.school.accountservice.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record AccountBalanceDto(
+    String accountNumber,
+    BigDecimal balance,
+    LocalDateTime updatedAt
+) {
+}
