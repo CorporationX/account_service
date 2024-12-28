2ebb9d685089dee58b4c5c285a6db4af9e0fa4b9
AlexLogino <alexeyjob3@gmail.com>
2024-12-28 21:11:15 +0300

    BJS2-41937 add migration V001

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,31 @@
--- Write your sql migration here!
+CREATE TABLE IF NOT EXISTS accounts_users (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number INT(20) UNIQUE NOT NULL CHECK account_number>=12,
+    owner VARCHAR DEFAULT user,
+    owner_id BIGINT NOT NULL,
+    type VARCHAR(128),
+    currency VARCHAR NOT NULL,
+    status VARCHAR DEFAULT active,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    closed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    version VARCHAR NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS accounts_projects (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number INT(20) UNIQUE NOT NULL CHECK account_number>=12,
+    owner VARCHAR DEFAULT project,
+    owner_id BIGINT NOT NULL,
+    type VARCHAR(128),
+    currency VARCHAR NOT NULL,
+    status VARCHAR DEFAULT active,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    closed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    version VARCHAR(20) NOT NULL,
+);
+
+CREATE UNIQUE INDEX owner_id_project_idx ON accounts_projects (owner_id);
+
+CREATE UNIQUE INDEX owner_id_user_idx ON accounts_users (owner_id);
