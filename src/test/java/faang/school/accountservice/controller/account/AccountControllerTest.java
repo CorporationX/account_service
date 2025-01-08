208e1a35e83657636968da39e66aa0aa8e457683
Daniyar Dostiev <daniyar.dst@gmail.com>
2025-01-09 04:16:30 +0700

    BJS2-41974-Работа с балансами. Решил мердж конфликты

--- src/test/java/faang/school/accountservice/controller/account/AccountControllerTest.java
+++ src/test/java/faang/school/accountservice/controller/account/AccountControllerTest.java
@@ -2,9 +2,9 @@ package faang.school.accountservice.controller.account;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.accountservice.dto.account.AccountDto;
-import faang.school.accountservice.entity.account.enums.AccountStatus;
-import faang.school.accountservice.entity.account.enums.AccountType;
-import faang.school.accountservice.enums.Currency;
+import faang.school.accountservice.enums.account.AccountStatus;
+import faang.school.accountservice.enums.account.AccountType;
+import faang.school.accountservice.enums.currency.Currency;
 import faang.school.accountservice.service.account.AccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
