8b281f09264cfb9c9337b0979df27f3b4332d4a6
Alexander Kuznetsov <adeskmath@gmail.com>
2025-01-11 21:57:39 +0300

    BJS2-41928 IT added for tariff

--- src/main/java/faang/school/accountservice/dto/savings/TariffDto.java
+++ src/main/java/faang/school/accountservice/dto/savings/TariffDto.java
@@ -1,7 +1,9 @@
 package faang.school.accountservice.dto.savings;
 
 import java.math.BigDecimal;
+import lombok.Builder;
 
+@Builder
 public record TariffDto(
     Long id,
     String title,
---
+++ src/test/java/faang/school/accountservice/controller/savings/SavingsAccountControllerMockMvcIT.java
@@ -0,0 +1,20 @@
+package faang.school.accountservice.controller.savings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SavingsAccountControllerMockMvcIT {
+
+  @Test
+  void addSavingsAccount() {
+  }
+
+  @Test
+  void getSavingsAccountById() {
+  }
+
+  @Test
+  void getSavingsAccountByOwnerId() {
+  }
+}
