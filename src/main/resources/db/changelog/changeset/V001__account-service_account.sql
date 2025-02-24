249702a6463517c65ed1adc3713077aa6af6946d
oxana <nov2303@mail.ru>
2025-02-24 21:53:45 +0500

    BJS2-53415 | Создание платежного счета

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,26 @@
 -- Write your sql migration here!
+CREATE TABLE account (
+    id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number     varchar(20) UNIQUE NOT NULL CHECK( LENGTH(number) BETWEEN 12 AND  20 ),
+    project_id bigint,
+    user_id    bigint,
+    type       smallint    NOT NULL,
+    currency   smallint    NOT NULL,
+    status     smallint    NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    closed_at  timestamptz DEFAULT CURRENT_TIMESTAMP,
+    version    smallint    NOT NULL DEFAULT 0
+);
+
+--Платежный счет имеет следующие параметры:
+--номер - строка от 12 до 20 цифр
+--владельца - учесть, что владельцем может быть как пользователь, таки. проект
+--тип - можно придумать несколько, например, расчетный счет для физ./юр. лиц, валютный счёт, и т.д
+--валюта - RUB, EUR, USD, …
+--статус - аккаунт может быть действующим, замороженным, закрытым
+--время создания
+--время изменения
+--время закрытия
+--версия счёта
+
