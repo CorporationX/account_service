15d83e1b320c21d13fd1889e508862edeefac2bb
Maksim Russkikh <15maksim@mail.ru>
2025-02-24 11:56:23 +0300

    save test

--- src/main/java/faang/school/accountservice/service/balance/audit/BalanceAuditService.java
+++ src/main/java/faang/school/accountservice/service/balance/audit/BalanceAuditService.java
@@ -4,7 +4,7 @@ import faang.school.accountservice.dto.balance.BalanceDto;
 import faang.school.accountservice.dto.balance.PaymentDto;
 
 public interface BalanceAuditService {
-    public void addAuditFromNewBalance(BalanceDto balanceDto);
+    void addAuditFromNewBalance(BalanceDto balanceDto);
 
-    public void addAuditFromExistingBalance(BalanceDto balanceDto, PaymentDto paymentDto);
+    void addAuditFromExistingBalance(BalanceDto balanceDto, PaymentDto paymentDto);
 }
