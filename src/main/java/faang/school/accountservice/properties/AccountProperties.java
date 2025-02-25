12c876607971741c3798bca566dd170216e1162e
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-25 20:46:44 +0300

    BJS2-53398: fix

--- src/main/java/faang/school/accountservice/properties/AccountProperties.java
+++ src/main/java/faang/school/accountservice/properties/AccountProperties.java
@@ -4,16 +4,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.math.BigDecimal;
-
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "account")
 public class AccountProperties {
 
     private int validityPeriod;
-
-    private BigDecimal startBonus;
 
     private Number number;
 
