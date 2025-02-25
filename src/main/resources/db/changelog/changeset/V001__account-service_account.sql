a1bb6153f9fca5ca16c23373a176a922285d3cdd
OxanaNovozhilova <nov2303@mail.ru>
2025-02-25 15:08:06 +0500

    BJS2-53415 | Создание платежного счета

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS account;
 CREATE TABLE account (
     id              BIGSERIAL   PRIMARY KEY,
     account_number  varchar(20) UNIQUE NOT NULL CHECK( LENGTH(account_number) BETWEEN 12 AND  20 ),
-    owner_id        BIGSERIAL   NOT NULL,
+    owner_id        BIGINT   NOT NULL,
     owner_type      VARCHAR(10) NOT NULL,
     account_type    VARCHAR(50) NOT NULL,
     currency        VARCHAR(5)  NOT NULL,
