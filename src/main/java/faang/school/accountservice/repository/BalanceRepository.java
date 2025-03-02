8612dae87200703920ff87d20f96ec23188848a8
Alexey Gorokhov <almigor@mail.ru>
2025-03-02 22:27:53 +0300

    Подготовка к cross-review

--- src/main/java/faang/school/accountservice/repository/BalanceRepository.java
+++ src/main/java/faang/school/accountservice/repository/BalanceRepository.java
@@ -1,10 +1,10 @@
 package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.Balance;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface BalanceRepository extends CrudRepository<Balance, Long> {
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
     Optional<Balance> findByAccountId(Long accountId);
 }
