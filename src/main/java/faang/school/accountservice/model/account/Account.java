3dc72305b51221dad46eb4d3d07bdae19581f1bd
therealadik <vladerm2000@yandex.ru>
2025-03-03 11:57:59 +0500

    ifx

--- src/main/java/faang/school/accountservice/model/account/Account.java
+++ src/main/java/faang/school/accountservice/model/account/Account.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
