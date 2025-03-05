4f69eaac42c5c23853531eaed6cdea4ea838bb78
Alexey Gorokhov <almigor@mail.ru>
2025-03-05 23:37:49 +0300

    Подготовка к cross-review

--- src/main/java/faang/school/accountservice/entity/Account.java
+++ src/main/java/faang/school/accountservice/entity/Account.java
@@ -1,9 +1,9 @@
 package faang.school.accountservice.entity;
 
+import faang.school.accountservice.enums.AccountStatus;
 import faang.school.accountservice.enums.AccountType;
 import faang.school.accountservice.enums.Currency;
 import faang.school.accountservice.enums.OwnerType;
-import faang.school.accountservice.enums.AccountStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
