c091fd50a7e55e183a8bfd0ae523077d21529b29
Dmitriy0010 <dda20040609@gmail.com>
2025-03-05 22:03:44 +0300

    db

--- src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/faang/school/accountservice/AccountServiceApplicationTests.java
@@ -2,11 +2,36 @@ package faang.school.accountservice;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 
-class AccountServiceApplicationTests {
+@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(classes = AccountServiceApplicationTests.class)
+public class AccountServiceApplicationTests {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.2")
+            .withDatabaseName("test_db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Test
     void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
+
     }
 }
+
