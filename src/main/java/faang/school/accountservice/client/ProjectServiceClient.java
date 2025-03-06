e8a0874faabd5e4518f50d455717335871b16b4f
rostex <rostex2@yandex.ru>
2025-03-06 22:16:33 +0300

    Добавлена проверка поднятия Spring контекста

--- src/main/java/faang/school/accountservice/client/ProjectServiceClient.java
+++ src/main/java/faang/school/accountservice/client/ProjectServiceClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "project-service")
+@FeignClient(
+        name = "project-service",
+        url = "${project-service.host}:${project-service.port}"
+)
 public interface ProjectServiceClient {
     @GetMapping("/projects/{projectId}")
     ProjectReadDto getProject(@PathVariable long projectId);
--- src/main/java/faang/school/accountservice/client/UserServiceClient.java
+++ src/main/java/faang/school/accountservice/client/UserServiceClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(
+        name = "user-service",
+        url = "${user-service.host}:${user-service.port}/users/api"
+)
 public interface UserServiceClient {
     @GetMapping("/users/{userId}")
     UserDto getUser(@PathVariable long userId);
