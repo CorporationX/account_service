09495b10ee0abbd0c78f7bde824ad16e1048675e
kaktus <dda20040609@gmail.com>
2025-03-11 22:32:30 +0300

    fix checkstyle

--- src/main/java/faang/school/accountservice/exception/NoFreeAccountNumbersException.java
+++ src/main/java/faang/school/accountservice/exception/NoFreeAccountNumbersException.java
@@ -1,6 +1,6 @@
 package faang.school.accountservice.exception;
 
-public class NoFreeAccountNumbersException extends IllegalStateException{
+public class NoFreeAccountNumbersException extends IllegalStateException {
     public NoFreeAccountNumbersException(String message) {
         super(message);
     }
