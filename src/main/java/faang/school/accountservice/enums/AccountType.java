058b0cca33e22403c8530c753e3cc67e1a4df137
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2025-03-15 18:33:51 +0300

    BJS2-53384 | Создание платежного счета

---
+++ src/main/java/faang/school/accountservice/enums/AccountType.java
@@ -0,0 +1,9 @@
+package faang.school.accountservice.enums;
+
+public enum AccountType {
+        CURRENT_ACCOUNT_FOR_INDIVIDUALS,
+        CURRENT_ACCOUNT_FOR_LEGAL_ENTITIES,
+        CURRENCY_ACCOUNT,
+        SAVINGS_ACCOUNT,
+        JOINT_ACCOUNT
+}
