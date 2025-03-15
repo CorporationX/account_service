b6d5885a00fccacef36d7cfa363fbdc8136accab
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2025-03-15 19:34:06 +0400

    Revert "BJS2-53384 | Создание платежного счета"
    This reverts commit 058b0cca33e22403c8530c753e3cc67e1a4df137.

--- src/main/java/faang/school/accountservice/enums/AccountStatus.java
+++
@@ -1,7 +0,0 @@
-package faang.school.accountservice.enums;
-
-public enum AccountStatus {
-        ACTIVE,
-        FROZEN,
-        CLOSED
-}
--- src/main/java/faang/school/accountservice/enums/AccountType.java
+++
@@ -1,9 +0,0 @@
-package faang.school.accountservice.enums;
-
-public enum AccountType {
-        CURRENT_ACCOUNT_FOR_INDIVIDUALS,
-        CURRENT_ACCOUNT_FOR_LEGAL_ENTITIES,
-        CURRENCY_ACCOUNT,
-        SAVINGS_ACCOUNT,
-        JOINT_ACCOUNT
-}
