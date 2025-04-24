34661b892a234f34605202fc283185b562de015d
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-04-25 04:25:33 +0500

    миграция в для account_service!

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,18 @@
--- Write your sql migration here!
+CREATE TABLE account (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number VARCHAR(20) NOT NULL
+    CHECK(
+    CHARACTER_LENGTH(number) BETWEEN 12 AND 20 AND number NOT LIKE '%[^0-9]%'),
+    owner_id  BIGINT NOT NULL,
+    owner_type VARCHAR(50) NOT NULL
+    CHECK( owner_type IN ('USER','PROJECT') ),
+    type VARCHAR(50)  NOT NULL,
+    currency VARCHAR(3) NOT NULL CHECK ( currency IN ('RUB','USD','EUR') ),
+    status varchar(50) NOT NULL CHECK ( status IN ('ACTIVE','BLOCKED','CLOSED') ),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    closed_at TIMESTAMP WITH TIME ZONE,
+    version INTEGER DEFAULT 0 NOT NULL
+);
+
+CREATE INDEX idx_account_owner ON account(owner_type, owner_id);
