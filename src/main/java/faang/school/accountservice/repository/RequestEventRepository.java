270e45736e04d92eb6c6c022da3d28c095354b45
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-04-26 12:29:56 +0300

    fix: Added Repository annotations

--- src/main/java/faang/school/accountservice/repository/RequestEventRepository.java
+++ src/main/java/faang/school/accountservice/repository/RequestEventRepository.java
@@ -2,10 +2,12 @@ package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.RequestEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 import java.util.List;
 
+@Repository
 public interface RequestEventRepository extends JpaRepository<RequestEvent, UUID> {
     List<RequestEvent> findAllByOrderByCreatedAtAsc();
 }
