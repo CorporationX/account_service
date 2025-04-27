de7476055fbe5ef4e3aa43c507a7c622af61599c
Anton Kapshukov <kapshukov.04@mail.ru>
2025-04-27 19:34:16 +0300

    BJS2-66560: избежал дублирование кода в дтошках

--- src/main/java/faang/school/accountservice/dto/savingsaccount/DepositDto.java
+++ src/main/java/faang/school/accountservice/dto/savingsaccount/DepositDto.java
@@ -1,18 +1,9 @@
 package faang.school.accountservice.dto.savingsaccount;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
-import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class DepositDto {
-    @NotBlank(message = "Savings account id is required")
-    private Long savingsAccountId;
-
-    @NotNull(message = "Amount is required")
-    @DecimalMin(value = "10.0", inclusive = false, message = "Amount must be bigger than 10")
-    private BigDecimal amount;
+@EqualsAndHashCode(callSuper = true)
+public class DepositDto extends BaseTransactionDto {
 }
