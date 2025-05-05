7c57cc3050ad8c1fbd769a638e1bd0c43f268123
Max Goloviznin <roxet59rus@mail.ru>
2025-05-06 00:41:05 +0500

    (BJS2-65732) quick migration fix

--- src/main/resources/db/changelog/changeset/V002__add_tables_link_savings_account.sql
+++ src/main/resources/db/changelog/changeset/V002__add_tables_link_savings_account.sql
@@ -1,6 +1,6 @@
-CREATE INDEX idx_account_owner_id ON account(owner_id);
+CREATE INDEX IF NOT EXISTS idx_account_owner_id ON account(owner_id);
 
-CREATE TABLE savings_account (
+CREATE TABLE IF NOT EXISTS savings_account (
     account_id bigint PRIMARY KEY,
     account_number varchar(128) NOT NULL UNIQUE,
     balance decimal NOT NULL default 0,
@@ -11,19 +11,19 @@ CREATE TABLE savings_account (
     CONSTRAINT fk_account_id FOREIGN KEY (account_id) REFERENCES account (id)
 );
 
-CREATE INDEX idx_created_at ON savings_account (created_at);
-CREATE INDEX idx_last_interest_accrual_at ON savings_account (last_interest_accrual_at);
+CREATE INDEX IF NOT EXISTS idx_created_at ON savings_account (created_at);
+CREATE INDEX IF NOT EXISTS idx_last_interest_accrual_at ON savings_account (last_interest_accrual_at);
 
-CREATE TABLE tariff (
+CREATE TABLE IF NOT EXISTS tariff (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     type_name varchar(128) NOT NULL UNIQUE,
     created_at timestamptz DEFAULT current_timestamp,
     updated_at timestamptz DEFAULT current_timestamp
 );
 
-CREATE INDEX idx_type_name ON tariff (type_name);
+CREATE INDEX IF NOT EXISTS idx_type_name ON tariff (type_name);
 
-CREATE TABLE tariff_history (
+CREATE TABLE IF NOT EXISTS tariff_history (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     tariff_id bigint NOT NULL,
     savings_account_id bigint NOT NULL,
@@ -33,10 +33,10 @@ CREATE TABLE tariff_history (
     CONSTRAINT fk_savings_account_id FOREIGN KEY (savings_account_id) REFERENCES savings_account (account_id)
 );
 
-CREATE INDEX idx_tariff_id ON tariff_history (tariff_id);
-CREATE INDEX idx_tariff_history_by_account ON tariff_history (savings_account_id, applied_at DESC);
+CREATE INDEX IF NOT EXISTS idx_tariff_id ON tariff_history (tariff_id);
+CREATE INDEX IF NOT EXISTS idx_tariff_history_by_account ON tariff_history (savings_account_id, applied_at DESC);
 
-CREATE TABLE tariff_rate_history (
+CREATE TABLE IF NOT EXISTS tariff_rate_history (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     tariff_id bigint NOT NULL,
     rate decimal(5, 2) NOT NULL,
@@ -45,4 +45,4 @@ CREATE TABLE tariff_rate_history (
     CONSTRAINT fk_tariff_history_id FOREIGN KEY (tariff_id) REFERENCES tariff (id)
 );
 
-CREATE INDEX idx_tariff_rate_history ON tariff_rate_history (tariff_id, changed_at DESC);
+CREATE INDEX IF NOT EXISTS idx_tariff_rate_history ON tariff_rate_history (tariff_id, changed_at DESC);
