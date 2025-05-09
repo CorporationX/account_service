5779d8b8fd496049d13fa4179613a184e36864a6
Max Goloviznin <roxet59rus@mail.ru>
2025-05-10 02:02:54 +0500

    (BJS2-65733) add some structure

--- src/main/java/faang/school/accountservice/listener/PaymentOperationEventListener.java
+++ src/main/java/faang/school/accountservice/listener/PaymentOperationEventListener.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.accountservice.dto.event.PaymentOperationEvent;
 import faang.school.accountservice.exception.JsonDeserializationException;
+import faang.school.accountservice.handler.PaymentOperationEventHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,13 @@ import org.springframework.stereotype.Component;
 public class PaymentOperationEventListener {
 
     private final ObjectMapper objectMapper;
+    private final PaymentOperationEventHandler eventHandler;
 
     public void receive(String message) {
         try {
             log.debug("Received new hashtag event: {}", message);
             PaymentOperationEvent event = objectMapper.readValue(message, PaymentOperationEvent.class);
-            // вызов метода обработки
+            eventHandler.handle(event);
         } catch (JsonProcessingException e) {
             throw new JsonDeserializationException("Deserialization json %s to event object error", message);
         }
---
+++ src/main/java/faang/school/accountservice/service/PaymentRequestService.java
@@ -0,0 +1,17 @@
+package faang.school.accountservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PaymentRequestService {
+
+    public void findPaymentRequestByToken(UUID token) {
+
+    }
+}
