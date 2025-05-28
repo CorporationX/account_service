387c83e7c5fedd93e293092ea9e1987944b72bd2
Artpyak <112198749+Artpyak@users.noreply.github.com>
2025-05-28 22:51:41 +0300

    feature/BJS2-66546 Добавление миграции (#477)
    * Добавление миграции
    
    * Правки по комментариям

---
+++ src/main/resources/db/changelog/changeset/account_V003_create_balance_table.sql
@@ -0,0 +1,23 @@
+CREATE SEQUENCE sequence_balance_id
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE balance
+(
+    id                 BIGINT PRIMARY KEY DEFAULT nextval('sequence_balance_id'),
+    account_id         BIGINT         NOT NULL UNIQUE,
+    authorized_balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
+    actual_balance     DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
+    created_at         TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at         TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    version            INTEGER        NOT NULL DEFAULT 1,
+    FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE,
+    CONSTRAINT positive_version CHECK (version >= 0),
+    CONSTRAINT positive_authorized_balance CHECK (authorized_balance >= 0),
+    CONSTRAINT positive_actual_balance CHECK (actual_balance >= 0)
+);
+
+CREATE INDEX idx_balance_account_id ON balance(account_id);
