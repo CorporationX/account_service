37dbe3c0799b1c6ff12cc2c099c55054ce1aca1d
pirojok_yt <tamik34@mail.ru>
2025-06-24 11:47:48 +0500

    finished task

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,14 @@
--- Write your sql migration here!
+CREATE TABLE accounts (
+    id bigint primary key,
+    balance DECIMAL(19, 4) NOT NULL DEFAULT 0.0,
+    owner VARCHAR(16) NOT NULL,
+    owner_id bigint NOT NULL,
+    type VARCHAR(32) NOT NULL,
+    currency VARCHAR(16) NOT NULL,
+    status VARCHAR(16) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    version VARCHAR(16)
+);
+
+CREATE INDEX owner_idx ON accounts(owner_id)
