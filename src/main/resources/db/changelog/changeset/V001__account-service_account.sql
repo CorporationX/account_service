9ec9e4d86983fa52a4ad6dac0b5573cd8f7ae33c
pirojok_yt <tamik34@mail.ru>
2025-06-25 18:04:33 +0500

    fixes

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,16 +1,16 @@
-CREATE SEQUENCE account_id_seq START 0;
+CREATE SEQUENCE account_id_seq START 1;
 CREATE TABLE account (
     id BIGINT PRIMARY KEY DEFAULT nextval('account_id_seq'),
     number VARCHAR(20) NOT NULL,
     owner_type VARCHAR(16) NOT NULL,
-    owner_id bigint NOT NULL,
+    owner_id BIGINT NOT NULL,
     type VARCHAR(32) NOT NULL,
     currency VARCHAR(16) NOT NULL,
     status VARCHAR(16) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     closed_at TIMESTAMP,
     version VARCHAR(16)
-) AUTO_INCREMENT = 0;
+);
 
-CREATE INDEX owner_idx ON account(number)
+CREATE INDEX number_idx ON account(number);
