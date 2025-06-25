feeff594722b2914faebdb948049bfebfe1b687f
pirojok_yt <tamik34@mail.ru>
2025-06-25 15:33:49 +0500

    fixes

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,7 +1,8 @@
+CREATE SEQUENCE account_id_seq START 0;
 CREATE TABLE account (
-    id bigint AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT PRIMARY KEY DEFAULT nextval('account_id_seq'),
     number VARCHAR(20) NOT NULL,
-    ownerType VARCHAR(16) NOT NULL,
+    owner_type VARCHAR(16) NOT NULL,
     owner_id bigint NOT NULL,
     type VARCHAR(32) NOT NULL,
     currency VARCHAR(16) NOT NULL,
