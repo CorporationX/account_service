78c3e559b6d687a479e24042275727251669bf4e
pirojok_yt <tamik34@mail.ru>
2025-06-25 17:30:17 +0500

    fixes

--- src/main/java/faang/school/accountservice/dto/AccountDto.java
+++ src/main/java/faang/school/accountservice/dto/AccountDto.java
@@ -5,7 +5,7 @@ import faang.school.accountservice.enums.Currency;
 
 public record AccountDto(
         Long id,
-        Account.OwnerType owner,
+        Account.OwnerType ownerType,
         long ownerId,
         Account.Type type,
         Currency currency,
--- src/main/java/faang/school/accountservice/mapper/AccountMapper.java
+++ src/main/java/faang/school/accountservice/mapper/AccountMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
-    @Mapping(target = "balance", ignore = true)
     @Mapping(target = "status", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
