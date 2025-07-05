e053811f249674daf56a67b328f96eacb39a184e
pirojok_yt <tamik34@mail.ru>
2025-07-05 15:35:40 +0500

    rebasing

--- src/main/java/faang/school/accountservice/entity/AccountNumberSequence.java
+++ src/main/java/faang/school/accountservice/entity/AccountNumberSequence.java
@@ -7,8 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigInteger;
-
 @Data
 @Builder
 @AllArgsConstructor
@@ -22,7 +20,7 @@ public class AccountNumberSequence {
     private Long id;
 
     @Column(name = "number", length = 20, nullable = false)
-    private BigInteger number;
+    private String number;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
