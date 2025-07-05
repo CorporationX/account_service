76fb3c35a9efefb0475c7de5e1d30665704926c0
Buga Dan <danbil.942@gmail.com>
2025-07-05 18:02:39 +0300

    first

---
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
@@ -0,0 +1,14 @@
+package faang.school.accountservice.service;
+
+import faang.school.accountservice.model.AccountType;
+
+public class FreeAccountNumbersService {
+
+    public void createNewAccNumbers(AccountType type, int quantity) {
+
+    }
+
+    public void createTargetQuantityOfAccNumbers(AccountType type, int targetQuantity) {
+
+    }
+}
