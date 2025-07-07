d0d9a219583706aa6381a7ec288dac0d6f143030
pirojok_yt <tamik34@mail.ru>
2025-07-07 18:10:16 +0500

    finished tests

--- src/main/java/faang/school/accountservice/repository/AccountNumberSequenceRepository.java
+++ src/main/java/faang/school/accountservice/repository/AccountNumberSequenceRepository.java
@@ -1,7 +1,6 @@
 package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.AccountNumberSequence;
-import faang.school.accountservice.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,22 +10,16 @@ public interface AccountNumberSequenceRepository extends JpaRepository<AccountNu
     @Modifying
     @Query(value = """
     UPDATE account_number_sequence
-    SET number = CAST(CAST(number AS BIGINT) + :value AS VARCHAR)
-    WHERE account_type = :type
+    SET number = number + :value
+    WHERE type = :type
     """, nativeQuery = true)
-    void tryIncrement(@Param("type") AccountType type, @Param("value") int value);
-
-    @Query(value = """
-    INSERT INTO account_number_sequence (number, type)
-    VALUES (:number, :type)
-    """, nativeQuery = true)
-    void createNewCounter(@Param("type") AccountType type, @Param("number") String initialNumber);
+    void tryIncrement(@Param("type") String type, @Param("value") long value);
 
     @Query(value = """
     SELECT * FROM account_number_sequence
     WHERE type = :type
     """, nativeQuery = true)
-    AccountNumberSequence getByType(@Param("type") AccountType type);
+    AccountNumberSequence getByType(@Param("type") String type);
 
-    boolean existsByType(AccountType type);
+    boolean existsByType(String type);
 }
