675e42e785b36a8ca327166c48b8773217a22334
Hitow22 <Hitow22@gmail.com>
2025-07-09 13:08:52 +0300

    message fix

--- src/main/java/faang/school/accountservice/dto/BalanceDto.java
+++ src/main/java/faang/school/accountservice/dto/BalanceDto.java
@@ -8,8 +8,8 @@ import java.math.BigDecimal;
 public record BalanceDto(
         @NotNull(message = "Balance id cannot be null")
         Long id,
-        @NotNull(message = "Authorization balance id cannot be null")
+        @NotNull(message = "Authorization balance cannot be null")
         BigDecimal authorizationBalance,
-        @NotNull(message = "Actual balance id cannot be null")
+        @NotNull(message = "Actual balance cannot be null")
         BigDecimal actualBalance) {
 }
