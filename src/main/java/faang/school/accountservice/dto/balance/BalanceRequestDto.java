49aae85c9b259d278025bf0ea872cf775d71e7ac
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-07-11 22:27:10 +0300

    BJS-78322 save commit

--- src/main/java/faang/school/accountservice/dto/balance/BalanceRequestDto.java
+++ src/main/java/faang/school/accountservice/dto/balance/BalanceRequestDto.java
@@ -1,5 +1,6 @@
 package faang.school.accountservice.dto.balance;
 
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
@@ -21,7 +22,7 @@ public class BalanceRequestDto {
 
     @NotNull(message = "amount is required")
     @DecimalMin(value = "1", message = "Amount must be at least 1")
-    @Digits(integer = 20, fraction = 0,
-            message = "Amount must be a whole number")
+    @DecimalMax(value = "10000000", message = "Amount must be at most 10 000 000")
+    @Digits(integer = 20, fraction = 0, message = "Amount must be a whole number")
     BigDecimal amount;
 }
