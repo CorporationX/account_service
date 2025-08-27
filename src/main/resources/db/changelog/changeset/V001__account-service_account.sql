a538736c8a4b950b929db86e91dc244b08276e08
Ivi-Anastasia <qwwsoul@gmail.com>
2025-08-27 21:51:14 +0100

    account table migration - corrected (didn't run before this correction, so the table should be fine)

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,15 +1,16 @@
 CREATE TABLE account (
-    id bigserial PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     number varchar(20) UNIQUE NOT NULL,
     owner_type varchar(32) NOT NULL,
     owner_id bigint NOT NULL,
     type varchar(64) NOT NULL,
     currency varchar(4) NOT NULL,
     status varchar(32) NOT NULL,
-    created_at timestampz NOT NULL DEFAULT now(),
-    updated_at timestampz not null,
-    closed_at timestampz,
-    version bigint NOT NULL,
+    balance numeric(19, 2) NOT NULL DEFAULT 0,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL,
+    closed_at timestamptz,
+    version bigint NOT NULL
+);
 
-    CREATE INDEX idx_account_owner ON account(owner_type, owner_id);
-)
+CREATE INDEX idx_account_owner ON account(owner_type, owner_id);
