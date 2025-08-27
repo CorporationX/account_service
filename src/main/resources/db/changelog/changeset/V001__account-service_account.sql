c43f574859148c844482d9f5943bd028db090276
Ivi-Anastasia <qwwsoul@gmail.com>
2025-08-27 02:03:15 +0100

    account table migration

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,15 @@
--- Write your sql migration here!
+CREATE TABLE account (
+    id bigserial PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number varchar(20) UNIQUE NOT NULL,
+    owner_type varchar(32) NOT NULL,
+    owner_id bigint NOT NULL,
+    type varchar(64) NOT NULL,
+    currency varchar(4) NOT NULL,
+    status varchar(32) NOT NULL,
+    created_at timestampz NOT NULL DEFAULT now(),
+    updated_at timestampz not null,
+    closed_at timestampz,
+    version bigint NOT NULL,
+
+    CREATE INDEX idx_account_owner ON account(owner_type, owner_id);
+)
