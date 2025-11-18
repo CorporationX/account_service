6a16b15b93b7fded4b3dd1c1229a5a29f2579d32
Mikhail <loginovm@ster.me>
2025-11-18 12:44:32 +0300

    added database migration + entity

---
+++ src/main/java/faang/school/accountservice/entity/Account.java
@@ -0,0 +1,92 @@
+package faang.school.accountservice.entity;
+
+import faang.school.accountservice.enums.AccountStatus;
+import faang.school.accountservice.enums.AccountType;
+import faang.school.accountservice.enums.Currency;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number", length = 20, nullable = false, unique = true)
+    private String accountNumber;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private AccountType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency", nullable = false)
+    private Currency currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private AccountStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "closed_at")
+    private LocalDateTime closedAt;
+
+    @Column(name = "frozen_at")
+    private LocalDateTime frozenAt;
+
+    @Column(name = "blocked_at")
+    private LocalDateTime blockedAt;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Long version;
+
+    @Column(name = "balance", precision = 19, scale = 4, nullable = false)
+    @Builder.Default
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "close_reason", length = 255)
+    private String closeReason;
+
+    @Column(name = "frozen_reason", length = 255)
+    private String frozenReason;
+
+    @Column(name = "block_reason", length = 255)
+    private String blockReason;
+}
--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,22 @@
--- Write your sql migration here!
+CREATE TABLE account (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    account_number varchar(20) UNIQUE NOT NULL,
+    user_id bigint NOT NULL,
+    type varchar(50) NOT NULL,
+    currency varchar(3) NOT NULL,
+    status varchar(50) NOT NULL,
+    created_at timestamptz DEFAULT current_timestamp,
+    updated_at timestamptz DEFAULT current_timestamp,
+    closed_at timestamptz,
+    frozen_at timestamptz,
+    blocked_at timestamptz,
+    version bigint NOT NULL DEFAULT 0,
+    balance numeric(19, 4) NOT NULL DEFAULT 0,
+    description varchar(255),
+    close_reason varchar(255),
+    frozen_reason varchar(255),
+    block_reason varchar(255),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE INDEX idx_account_user_id ON account (user_id);
