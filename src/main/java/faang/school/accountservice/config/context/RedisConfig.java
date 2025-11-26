ab56405c6016f37e3d18acffccafb62adb61e93e
Sofiia Сhebotareva <Sofiia.chebotareva@gmail.com>
2025-11-26 13:52:40 +0100

    fix after cross review, updted redis config

--- src/main/java/faang/school/accountservice/config/context/RedisConfig.java
+++ src/main/java/faang/school/accountservice/config/context/RedisConfig.java
@@ -1,13 +1,11 @@
 package faang.school.accountservice.config.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,31 +14,16 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        Jackson2JsonRedisSerializer<Object> jackson =
+                new Jackson2JsonRedisSerializer<>(new ObjectMapper(), Object.class);
 
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
 
-        template.setValueSerializer(jacksonSerializer());
-        template.setHashValueSerializer(jacksonSerializer());
+        template.setValueSerializer(jackson);
+        template.setHashValueSerializer(jackson);
 
         return template;
-    }
-
-    private RedisSerializer<Object> jacksonSerializer() {
-        Jackson2JsonRedisSerializer<Object> serializer =
-                new Jackson2JsonRedisSerializer<>(Object.class);
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.activateDefaultTyping(
-                LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL
-        );
-        mapper.findAndRegisterModules();
-
-        serializer.setObjectMapper(mapper);
-
-        return serializer;
-
     }
 }
