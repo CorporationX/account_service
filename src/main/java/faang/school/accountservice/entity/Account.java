790dc5da17050344be228ece435d9054ec42439a
Ruslan <ruslan8020110@gmail.com>
2025-11-29 15:57:54 +0300

    первая версия

---
+++ src/main/java/faang/school/accountservice/entity/Account.java
@@ -0,0 +1,73 @@
+package faang.school.accountservice.entity;
+
+import faang.school.accountservice.enums.AccountStatus;
+import faang.school.accountservice.enums.AccountType;
+import faang.school.accountservice.enums.Currency;
+import faang.school.accountservice.enums.OwnerType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "number", length = 20, unique = true, nullable = false)
+    private String number;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "owner_type", nullable = false, length = 10)
+    private OwnerType ownerType;
+
+    @Column(name = "owner_id", nullable = false)
+    private Long ownerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type", nullable = false, length = 20)
+    private AccountType accountType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency", nullable = false, length = 3)
+    private Currency currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 10)
+    private AccountStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "closed_at")
+    private LocalDateTime closedAt;
+
+    @Column(name = "version", nullable = false)
+    private int version;
+}
+
+
+
+
+
--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,17 @@
--- Write your sql migration here!
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE account (
+
+                         id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+                         number VARCHAR(20) NOT NULL UNIQUE,
+                         owner_type VARCHAR(10) NOT NULL,
+                         owner_id BIGINT NOT NULL,
+                         account_type VARCHAR(20) NOT NULL,
+                         currency VARCHAR(3) NOT NULL,
+                         status VARCHAR(10) NOT NULL,
+                         created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                         updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                         closed_at TIMESTAMP WITHOUT TIME ZONE,
+                         version INTEGER NOT NULL);
+
+CREATE INDEX idx_account_owner_type_id ON account (owner_type, owner_id);
