8fba12f93af1d1bb37c16e6149d135d1a13225e8
Ruslan <ruslan8020110@gmail.com>
2025-12-27 02:06:07 +0300

    доработки. Убрал неиспользуемый импорт

--- src/main/java/faang/school/accountservice/mapper/AccountMapper.java
+++ src/main/java/faang/school/accountservice/mapper/AccountMapper.java
@@ -3,13 +3,10 @@ package faang.school.accountservice.mapper;
 import faang.school.accountservice.dto.AccountDto;
 import faang.school.accountservice.entity.Account;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
     AccountDto toDto(Account account);
 
     Account toEntity(AccountDto dto);
-
-    void updateAccountFromDto(AccountDto dto, @MappingTarget Account entity);
 }
